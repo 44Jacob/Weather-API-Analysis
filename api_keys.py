488356ab61ce29f130f75c09f570263bd7df1d30
Jacob Altinisik <152909634+44Jacob@users.noreply.github.com>
2024-03-17 14:37:34 -0500

    deleted extra files

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "bf69c7688862919e72c881d7356846db"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "e544d1a5aa944d8fb0b552d745a04636"
